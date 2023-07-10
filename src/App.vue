3e863e55155474823487685d53376996ddd05c59
Dmitriy <markelovdn@gmail.com>
2023-07-10 10:54:43 +0300

    first commit clubcrm front

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <span>{{count}}</span>
+    <span>{{name}}</span>
+    <button @click="authStore.increment"></button>
+
+  </div>
+
+</template>
+
+<script setup>
+import { useAuthStore } from '@/store/auth.js'
+import { storeToRefs} from  'pinia'
+const authStore = useAuthStore()
+const { count } = storeToRefs(authStore)
+const { name } = storeToRefs(authStore)
+
+</script>
+
+<style scoped lang="scss">
+
+</style>
---
+++ src/store/auth.js
@@ -0,0 +1,18 @@
+import { defineStore } from "pinia";
+
+export const useAuthStore = defineStore('auth', {
+    state: () => ({
+        count: 0,
+        name: 'Eduardo'
+    }),
+
+    getters: {
+        doubleCount: (state) => state.count * 2,
+    },
+
+    actions: {
+        increment() {
+            this.count++
+        },
+    },
+})
