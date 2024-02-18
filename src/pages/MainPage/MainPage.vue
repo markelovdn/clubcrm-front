bfb01788205f4b062614dd247cbb13d9da6a14fa
Дмитрий <markelovdn@gmail.com>
2024-02-18 16:52:55 +0300

    test ci/cd

--- src/pages/MainPage/MainPage.vue
+++ src/pages/MainPage/MainPage.vue
@@ -14,20 +14,20 @@ onMounted(() => {
   <div class="main-container">
     <h1>Главная страница</h1>
     <div class="main-container__content">
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deleniti perferendis nulla veniam harum rerum, laborum
-      tempore consectetur excepturi magnam impedit dolorum distinctio consequuntur reiciendis voluptatem quis, ducimus,
-      vero non ut. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi eveniet, facilis nobis repellat
-      nulla sapiente soluta deserunt. Nobis aliquid molestiae vel, quia, eveniet rem tenetur voluptatum atque distinctio
-      corrupti ipsum? Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis tempora beatae magnam, ea
-      doloremque recusandae dignissimos consequatur dicta amet exercitationem enim nisi soluta repellat ullam? Vero
-      laboriosam ullam minus sint. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet beatae repudiandae
-      nihil illum voluptates. Atque repudiandae sed pariatur, dolores veritatis laudantium? Cum adipisci praesentium
-      animi provident aliquam, at ut quasi. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illum, sit
-      ducimus. Nihil voluptate nesciunt quaerat natus quos cupiditate facere blanditiis. Nemo minima veniam illum rerum
-      quam iure nihil possimus incidunt? Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deleniti perferendis
-      nulla veniam harum rerum, laborum tempore consectetur excepturi magnam impedit dolorum distinctio consequuntur
-      reiciendis voluptatem quis, ducimus, vero non ut. Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-      Eligendi eveniet, facilis nobis repellat nulla sapiente soluta deserunt. Nobis aliquid molestiae vel, quia,
+      Новый текст Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deleniti perferendis nulla veniam harum
+      rerum, laborum tempore consectetur excepturi magnam impedit dolorum distinctio consequuntur reiciendis voluptatem
+      quis, ducimus, vero non ut. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi eveniet, facilis
+      nobis repellat nulla sapiente soluta deserunt. Nobis aliquid molestiae vel, quia, eveniet rem tenetur voluptatum
+      atque distinctio corrupti ipsum? Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis tempora beatae
+      magnam, ea doloremque recusandae dignissimos consequatur dicta amet exercitationem enim nisi soluta repellat
+      ullam? Vero laboriosam ullam minus sint. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet beatae
+      repudiandae nihil illum voluptates. Atque repudiandae sed pariatur, dolores veritatis laudantium? Cum adipisci
+      praesentium animi provident aliquam, at ut quasi. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illum,
+      sit ducimus. Nihil voluptate nesciunt quaerat natus quos cupiditate facere blanditiis. Nemo minima veniam illum
+      rerum quam iure nihil possimus incidunt? Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deleniti
+      perferendis nulla veniam harum rerum, laborum tempore consectetur excepturi magnam impedit dolorum distinctio
+      consequuntur reiciendis voluptatem quis, ducimus, vero non ut. Lorem ipsum dolor sit amet, consectetur adipisicing
+      elit. Eligendi eveniet, facilis nobis repellat nulla sapiente soluta deserunt. Nobis aliquid molestiae vel, quia,
       eveniet rem tenetur voluptatum atque distinctio corrupti ipsum? Lorem ipsum dolor sit amet consectetur adipisicing
       elit. Veritatis tempora beatae magnam, ea doloremque recusandae dignissimos consequatur dicta amet exercitationem
       enim nisi soluta repellat ullam? Vero laboriosam ullam minus sint. Lorem, ipsum dolor sit amet consectetur
